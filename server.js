66ac032f974460f71716b0af5fbfaee6e5c2388a
sucukwurst <31952467+sucukwurst@users.noreply.github.com>
2020-05-12 02:22:24 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+var app = require('express')(); // Create an instance of an Express app
+
+var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+
+mobileApp.tables.add('Nutzer');
+mobileApp.tables.add('Schmerzbericht'); // Create a table for 'Book' with default settings
+
+app.use(mobileApp);
+app.listen(process.env.PORT || 3000);
